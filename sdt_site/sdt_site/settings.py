4f837c181b3ad8c82a601057026d58df007667c5
Jonathan Ramanujam <75343684+jonathanramanujam@users.noreply.github.com>
2022-11-21 20:10:10 -0700

    style updates

--- sdt_site/sdt_site/settings.py
+++ sdt_site/sdt_site/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'shaggydogtale/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'shaggydogtale/static')
 LOGIN_REDIRECT_URL = 'shaggydogtale:browse'
 LOGIN_URL = 'login'
